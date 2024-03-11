b53824c96cf1a171ffe5b060d0e2c6854a0758a3
s24904 <s24904@pjwstk.edu.pl>
2024-03-12 00:57:53 +0100

    Added function to count average

--- Zadanie1/Zadanie1/Program.cs
+++ Zadanie1/Zadanie1/Program.cs
@@ -2,9 +2,14 @@
 {
     public class Program
     {
-        public static void main(string[] args)
+        public static double average(int []tab)
         {
-            //AAAAA
+            int sum = 0;
+            foreach (int i in tab)
+            {
+                sum += i;
+            }
+            return sum/tab.Length;
         }
     }
 }
