e99662162b831214d1fcb7a78806c299a35070f9
s24904 <s24904@pjwstk.edu.pl>
2024-03-15 00:01:54 +0100

    added MaxValue method

--- Zadanie1/Zadanie1/Program.cs
+++ Zadanie1/Zadanie1/Program.cs
@@ -11,5 +11,32 @@
             }
             return sum/tab.Length;
         }
+
+        public static int MaxValue(int[] values)
+        {
+            if (values.Length>0)
+            {
+                int max = values[0];
+                foreach (int i in values)
+                {
+                    if (i>max)
+                    {
+                        max = i;
+                    }
+                }
+
+                return max;
+            }
+
+            return 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(average([1, 2, 3]));
+            Console.WriteLine(MaxValue([4, 5, 6]));
+            
+        }
+        
     }
 }
